4f9b9a85f82f93a28f6f0a072d303a7661f34113
Abhishek Garg <103814691+AbhishekGarg0507@users.noreply.github.com>
2024-04-05 02:38:02 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import "./home.css"
+
+const Social = () =>{
+    return(
+        <div className="home__social">
+    
+            <a href="https://github.com/AbhishekGarg0507" className="home__social-icon" target="_blank">
+                <i className="uil uil-github-alt"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/abhishek-garg-1400611bb/" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin"></i>
+            </a>
+
+        </div>
+    )
+}
+
+export default Social;
