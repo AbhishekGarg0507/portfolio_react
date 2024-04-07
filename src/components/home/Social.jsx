2e48d2eb87155248c1331368042dff35884f8e7e
Abhishek Garg <103814691+AbhishekGarg0507@users.noreply.github.com>
2024-04-08 01:46:55 +0530

    update

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -5,11 +5,11 @@ const Social = () =>{
     return(
         <div className="home__social">
     
-            <a href="https://github.com/AbhishekGarg0507" className="home__social-icon" target="_blank">
+            <a href="https://github.com/AbhishekGarg0507" className="home__social-icon"  target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-github-alt"></i>
             </a>
-
-            <a href="https://www.linkedin.com/in/abhishek-garg-1400611bb/" className="home__social-icon" target="_blank">
+            
+            <a href="https://www.linkedin.com/in/abhishek-garg-1400611bb/" className="home__social-icon"  target="_blank" rel="noopener noreferrer">
                 <i className="uil uil-linkedin"></i>
             </a>
 
