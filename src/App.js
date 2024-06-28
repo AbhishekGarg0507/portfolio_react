4b4ced77c59e8b1d487c100840024d9cfbac87e2
Abhishek Garg <103814691+AbhishekGarg0507@users.noreply.github.com>
2024-06-28 11:40:13 +0530

    work section added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
+import Work from './components/work/Work';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import Scrollup from './components/scrollup/Scrollup';
@@ -18,6 +19,7 @@ function App() {
       <About />
       <Skills />
       <Qualification />
+      <Work />
       <Contact />
       
     </main>
